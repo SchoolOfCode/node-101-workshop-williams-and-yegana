166e5d9ac1b1171c003f91eb0e25f35a21b781c2
YeganaAbd <abd.yegane@gmail.com>
2021-12-06 23:07:16 +0000

    All tasks

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-let myCollection = [
-  {
-    name: "School of Code mug",
-    count: 1,
-    whatILike: "It has my cute pixel character on it!",
-  },
-  {
-    name: "School of Code hat",
-    count: 2,
-    whatILike: "An often overlooked fashion accessory",
-  },
-  {
-    name: "School of Code pillow",
-    count: 1,
-    whatILike: "Eat. Sleep. Code. Repeat :)",
-  },
-];
-console.log(myCollection);
+import myCollection from "./collection.js";
+import chalk from "chalk";
+
+// Task 2c
+function describeItem(item) {
+  if (item.count === 1) {
+    console.log(
+      `I have a ${chalk.cyan(
+        item.name
+      )}. Here's what I like about it: ${chalk.green(item.whatILike)}`
+    );
+  } else {
+    console.log(
+      `I have ${chalk.yellow(item.count)} ${chalk.cyan(
+        item.name
+      )}'s Here's what I like about it: ${item.whatILike}`
+    );
+  }
+}
+describeItem(myCollection[1]);
+
+// Task 2d
+
+function describeCollection(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    describeItem(arr[i]);
+  }
+}
+describeCollection(myCollection);
+
+//Alternative forEach() solution
+//myCollection.forEach(describeItem);
